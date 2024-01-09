1676e213c480f2ae9a789df78d373728b6af2d25
David16240 <davidherbreteau16@gmail.com>
2024-01-09 01:22:51 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer>
+
+<nav>
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'menu-2',
+                'container'      => 'false',
+                'menu_class'     => 'cssFooter',
+            )
+        );
+        ?>
+        
+        <?php get_template_part('template-parts/modale'); ?>
+        <?php get_template_part('template-parts/lightbox'); ?>
+
+</nav>
+    
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
